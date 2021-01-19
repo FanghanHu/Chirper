cd81a7d51f06800ab6c70074bb342b44105c857b
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-19 14:50:02 -0600

    login with access code API

--- server.js
+++ server.js
@@ -3,17 +3,20 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const db = require(path.join(__dirname, './models'));
 const app = express();
+const routes = require("./routes");
 require("dotenv").config();
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
 // Define API routes here
+app.use(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
@@ -22,8 +25,9 @@ app.get("*", (req, res) => {
 });
 
 
+
 //sync models before active server
-db.sequelize.sync().then(() => {
+db.sequelize.sync({force: false}).then(() => {
   //bind server.
   app.listen(PORT, () => {
       console.log("Server is now listening on port: " + PORT);
