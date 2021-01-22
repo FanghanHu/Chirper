b5285bc1a6a96283563b179af04ac7cd078555bf
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-22 10:20:02 -0600

    removed unused code

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,6 @@ import CustomerInfoForm from "./components/CustomerInfoForm";
 
 function App() {
   return (
-    // <div className="App">
-    //   <div className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <h2>Welcome to React</h2>
-    //   </div>
-    //   <p className="App-intro">
-    //     To get started, edit <code>src/App.js</code> and save to reload.
-    //   </p>
-    // </div>
     <>
       <Router>
         <Route path="/" exact>
