0d65eaafde9c8b50b73c461865a8c45d5a29733d
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-24 15:35:07 -0600

    order page and ordering function call

--- client/src/components/OrderPage/index.js
+++ client/src/components/OrderPage/index.js
@@ -5,7 +5,13 @@ class OrderPage extends Component {
     constructor() {
         super();
         this.state = {
-            order: null,
+            //TODO: pass in the order creator, and table information, and customer info
+            order: {
+                creatorId:1,
+                Tables:[],
+                Customers:[],
+                OrderItems:[]
+            },
             server: null,
             menus: [{Items: []},{Items: []}]
         }
@@ -13,8 +19,51 @@ class OrderPage extends Component {
         this.loadMenus();
     }
 
-    orderItem = async (item) => {
-        //TODO:
+    /**
+     * order an item, changes made with this method must be saved with "saveOrder()" before it will effect server side data.
+     */
+    orderItem = (item) => {
+        //add the item to orderItem list
+        this.setState({
+            order: {
+                ...this.state.order,
+                OrderItems: [...this.state.order.OrderItems, {...item, status: "NEW"}]
+            }
+        })
+    }
+
+    /**
+     * send the current order to the server.
+     */
+    saveOrder = async () => {
+        const order = this.state.order;
+        if(order) {
+            let orderId = order.id;
+
+            //create a new order if the current order doesn't exist
+            if(!orderId) {
+                //TODO: creatorId
+                const result = await axios.post("/api/order/create/", {
+                        creatorId: 1
+                });
+                orderId = result.data.id;
+            }
+            
+            //find all items taht's newly ordered
+            const newItems = order.OrderItems.filter(item => item.status === "NEW");
+
+            //order new items through the API
+            //TODO: serverId
+            const result = await axios.post("/api/order/orderItem/", {
+                    orderId,
+                    items: newItems,
+                    serverId: 1
+            });
+
+            //TODO: allow modification of existing items
+
+            this.setState({order: result.data});
+        }
     }
     
     loadMenus = async () => {
@@ -24,6 +73,12 @@ class OrderPage extends Component {
             }
         });
         this.setState({menus: result.data});
+    }
+
+    placeOrderAndExit = async () => {
+        await this.saveOrder();
+
+        //TODO: exit the page
     }
 
     render() {
@@ -53,7 +108,11 @@ class OrderPage extends Component {
                         </div>
                     </div>
                     <div className="col-3">
-                        3
+                            <div className="d-flex justify-content-around flex-wrap">
+                                <button className="btn btn-success m-1" style={{width:"8em"}} onClick={this.saveOrder}>Place Order</button>
+                                <button className="btn btn-primary m-1" style={{width:"8em"}}>Payment</button>
+                                <button className="btn btn-danger m-1" style={{width:"8em"}}>Exit</button>
+                            </div>
                     </div>
                 </div>
             </div>
