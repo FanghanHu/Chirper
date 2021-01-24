714d5b00a0c2bf0e832f333734630c1378ce3ed3
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-23 20:51:57 -0600

    temp save

--- client/src/components/OrderPage/index.js
+++ client/src/components/OrderPage/index.js
@@ -5,21 +5,47 @@ class OrderPage extends Component {
         super();
         this.state = {
             order: null,
-            server: null
+            server: null,
+            sideBar: [],
+            mainMenu: []
         }
+    }
+
+    orderItem = async (item) => {
+        //TODO:
+    }
+
+    loadMenu = async (menuName) => {
+        //TODO:
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-3 bg-primary">
-                        1
+            <div className="container-fulid h-100">
+                <div className="row w-75 h-75 mx-auto my-5">
+                    <div className="col-3">
+                        <div className="card h-100">
+                            <div className="card-body bg-light">
+                                {this.state.sideBar.map(button => {
+                                    return (
+                                        <button className="btn btn-primary" onClick={() => { this.orderItem(button.item) }}>{button.item.ItemName}</button>
+                                    );
+                                })}
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-6 bg-warning">
-                        2
+                    <div className="col-6">
+                        <div className="card h-100">
+                            <div className="card-body  bg-light">
+                                {this.state.mainMenu.map(button => {
+                                    return (
+                                        <button className="btn btn-primary" onClick={() => { this.orderItem(button.item) }}>{button.item.ItemName}</button>
+                                    );
+                                })}
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-3 bg-success">
+                    <div className="col-3">
                         3
                     </div>
                 </div>
