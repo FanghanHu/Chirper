b5167468e6aeb9897db2d2fd721f2a3fd5b3c2a3
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-25 13:16:44 -0600

    CustomerList now uses customer context

--- client/src/components/CustomerList/index.js
+++ client/src/components/CustomerList/index.js
@@ -1,30 +1,31 @@
-import React, {Component}  from "react";
+import React from "react";
+import {useCustomer, useSetCustomer} from "../../Contexts/customer-context";
 
-class CustomerList extends Component {
-    render() {
-        return (
-            <ul className="list-unstyled text-center">
-                {
-                    this.props.customers.map(customer => {
-                        const text = `${customer.fullName} - ${customer.phone}`
-                        if(!this.props.filter || text.toLowerCase().includes(this.props.filter.toLowerCase()))
-                        {
-                            return (
-                                <li key={customer.id}>
-                                    <button className={"btn m-1 " + (this.props.selectedCustomer && (this.props.selectedCustomer.id===customer.id)?"btn-success":"btn-light")}  onClick={() => {this.props.selectCustomer(customer);}}>
-                                        {text}
-                                    </button>
-                                </li>
-                            );
-                        } else {
-                            return null;
-                        }
-                        
-                    })
-                }
-            </ul>
-        );
-    }
-}
 
-export default CustomerList;
+export default function CustomerList(props) {
+
+    const selectedCustomer = useCustomer();
+    const setCustomer = useSetCustomer();
+
+    return (
+        <ul className="list-unstyled text-center">
+            {
+                props.customers.map(customer => {
+                    const text = `${customer.fullName} - ${customer.phone}`
+                    if (!props.filter || text.toLowerCase().includes(props.filter.toLowerCase())) {
+                        return (
+                            <li key={customer.id}>
+                                <button className={"btn m-1 " + (selectedCustomer && (selectedCustomer.id === customer.id ) ? "btn-success" : "btn-light")} onClick={() => { setCustomer(customer); }}>
+                                    {text}
+                                </button>
+                            </li>
+                        );
+                    } else {
+                        return null;
+                    }
+
+                })
+            }
+        </ul>
+    );
+} 
