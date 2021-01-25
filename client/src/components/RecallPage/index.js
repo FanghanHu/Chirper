d6048e36bbf909fae081e5e6de1de1220d595d0a
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-25 14:18:06 -0600

    refactor recall page layout

--- client/src/components/RecallPage/index.js
+++ client/src/components/RecallPage/index.js
@@ -1,58 +1,53 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 
 function RecallPage() {
-
-
-
-
-
     return (
-        <>
-    <div className="card m-5">
-        <div className="card-header">
-            Orders
+        <div className="d-flex flex-column justify-content-center h-100">
+            <div className="card mx-auto w-75 h-75">
+                <div className="card-header">
+                    Orders
+                </div>
+                <div className="card-body">
+                    <div className="order-form" style={{ textAlign: "right" }}>
+                        <label htmlFor="order" className="mr-2">Order Number: </label>
+                        <input type="text" id="order"></input>
+                        <button className="btn btn-success m-1">Go</button>
+                    </div>
+                    <table className="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Order Number</th>
+                                <th scope="col">Table</th>
+                                <th scope="col">Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div style={{position:"absolute", bottom: "1em", right:"1em"}}>
+                        <button type="button" className="btn btn-success btn-lg float-right m-1" style={{width:"8em"}}>Open</button>
+                        <button type="button" className="btn btn-danger btn-lg float-right m-1" style={{width:"8em"}}>Exit</button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="card-body">
-        <div className="order-form" style={{textAlign: "right"}}>
-            <label for="order">Order #</label>
-            <input type="text" className="order" id="order#"></input>
-            <button className="btn-lg btn-success" type="go" style={{position: "relative", left:"17px"}}>Go</button>
-        </div>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Order #</th>
-                        <th scope="col">Table</th>
-                        <th scope="col">Price</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
-    <div className="container text-right"> 
-        <button type="button" className="btn btn-success btn-lg" style={{position: "relative" ,right: "25px" }}>Open</button>
-        <button type="button" className="btn btn-danger btn-lg" style={{position: "relative" ,left: "25px"}}>Exit</button>
-    </div>
-    </>
     )
 }
 
