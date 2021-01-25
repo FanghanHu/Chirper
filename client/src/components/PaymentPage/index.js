fe7ef34325659de23df10961d6bdc1177948e0e2
Alyssa Hollowell <alyssa.hollowell2@gmail.com>
2021-01-25 14:30:11 -0600

    added buttons in payment page

--- client/src/components/PaymentPage/index.js
+++ client/src/components/PaymentPage/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReceiptPreview from "../ReceiptPreview";
+import InputWithIcon from "../InputWithIcon";
 
 function PaymentPage() {
     return(
-    <>    
-    <div className="card m-5" style={{width: "23rem"}}>
+    <>   
+<div className="container"> 
+    <div className="row">
+        <div className="card m-5" style={{width: "23rem"}}>
         <ReceiptPreview order={{
             "id": 1,
             "orderNumber": "#1",
@@ -208,15 +211,87 @@ function PaymentPage() {
         }}/>
     </div>
 
-    <div className="card m-3" style={{float: "right"}}>
-            <div className="card-header">
+    <div className="card m-5" style={{width: "30rem"}}>
+            <div className="card-header" style={{fontSize: "20px"}}>
             Cash Payment:
             </div>
         <div className="card-body">
+            <div className="row mx-5" style={{margin: "20px"}}>
+                <div className="col-4 p-1">
+                <button className="btn btn-success">$10.00</button>
+                </div>
+                <div className="col-4 p-1">
+                <button className="btn btn-success">$10.00</button>
+                </div>
+                <div className="col-4 p-1">
+                <button className="btn btn-success">$10.00</button>
+                </div>
+                <div className="col-4 p-1">
+                <button className="btn btn-success">$10.00</button>
+                </div>
+                <div className="col-4 p-1">
+                <button className="btn btn-success">$10.00</button>
+                </div>
+                <div className="col-4 p-1">
+                <button className="btn btn-success">$10.00</button>
+                </div>
+            </div>
+            <div className="w-50 mx-auto">
+            <InputWithIcon label={"$"}/>
+            </div>
+
+            <div className="row mx-5">
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">1</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">2</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">3</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">4</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">5</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">6</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">7</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">8</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">9</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">Delete</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">0</button>
+                </div>
+                <div className="col-4 p-1">
+                    <button className="btn btn-light w-100 h-100">.</button>
+                </div>
+            </div>
+                <div className="row mx-5" style={{margin: "20px"}}>
+                    <button className="btn btn-primary">Pay Card</button>
+                    <button className="btn btn-success">Pay Cash</button>
+                </div>
+
         </div>
     </div>
 
+    </div>
+</div>
 
+        <div className="container text-right">
+            <button className="btn btn-danger">Exit</button>
+        </div>
     </>
     )
 }
