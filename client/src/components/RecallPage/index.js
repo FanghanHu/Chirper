980dd2a075d684dda2f04d43cd3b44d617d6587e
Fanghan Hu <Fanghan.Hu@gmail.com>
2021-01-25 22:21:08 -0600

    filped position of cancel and order button

--- client/src/components/RecallPage/index.js
+++ client/src/components/RecallPage/index.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import {Redirect, useHistory} from "react-router-dom";
-import {useUser} from "../../Contexts/user-context";
-import {useOrder, useSetOrder} from '../../Contexts/order-context'
+import { Redirect, useHistory } from "react-router-dom";
+import { useUser } from "../../Contexts/user-context";
+import { useOrder, useSetOrder } from '../../Contexts/order-context'
 
 
 function RecallPage() {
@@ -22,7 +22,7 @@ function RecallPage() {
                 eagarLoad: true
             }
         }).then((res) => {
-            if(mounted) {
+            if (mounted) {
                 setOrders(res.data);
             }
         })
@@ -36,11 +36,11 @@ function RecallPage() {
     //check if user is logged in
     const user = useUser();
     const history = useHistory();
-    if(!user) {
-        return <Redirect to="/"/>
+    if (!user) {
+        return <Redirect to="/" />
     }
 
-    
+
 
     return (
         <div className="d-flex flex-column justify-content-center h-100">
@@ -51,7 +51,7 @@ function RecallPage() {
                 <div className="card-body">
                     <div className="order-form" style={{ textAlign: "right" }}>
                         <label htmlFor="order" className="mr-2">Order Number: </label>
-                        <input type="text" value={orderNumber} onChange={(e) => {setOrderNumber(e.target.value)}}></input>
+                        <input type="text" value={orderNumber} onChange={(e) => { setOrderNumber(e.target.value) }}></input>
                         <button className="btn btn-success m-1" onClick={() => {
                             axios.get('/api/order/getWithOrderNumber', {
                                 params: {
@@ -78,26 +78,26 @@ function RecallPage() {
                         <tbody>
                             {orders.map(el => {
                                 return (
-                                    <tr key={el.id} className={order && (order.id===el.id)?"bg-success":""} onClick={() => {
+                                    <tr key={el.id} className={order && (order.id === el.id) ? "bg-success" : ""} onClick={() => {
                                         setOrder(el);
                                     }}>
                                         <td>{el.orderNumber}</td>
-                                        <td>{el.Tables[0]?el.Tables[0].tableName : ""}</td>
-                                        <td>{el.Customers[0]?el.Customers[0].fullName : ""}</td>
+                                        <td>{el.Tables[0] ? el.Tables[0].tableName : ""}</td>
+                                        <td>{el.Customers[0] ? el.Customers[0].fullName : ""}</td>
                                         <td>{el.creator.fullName}</td>
                                     </tr>
                                 );
                             })}
                         </tbody>
                     </table>
-                    <div style={{position:"absolute", bottom: "1em", right:"1em"}}>
-                        <button type="button" className="btn btn-success btn-lg float-right m-1" style={{width:"8em"}} onClick={() => {
-                            history.push('/orderPage');
-                        }}>Open</button>
-                        <button type="button" className="btn btn-danger btn-lg float-right m-1" style={{width:"8em"}} onClick={() => {
+                    <div style={{ position: "absolute", bottom: "1em", right: "1em" }}>
+                        <button type="button" className="btn btn-danger btn-lg float-right m-1" style={{ width: "8em" }} onClick={() => {
                             setOrder(null);
                             history.push('/mainMenu');
                         }}>Exit</button>
+                        <button type="button" className="btn btn-success btn-lg float-right m-1" style={{ width: "8em" }} onClick={() => {
+                            history.push('/orderPage');
+                        }}>Open</button>
                     </div>
                 </div>
             </div>
